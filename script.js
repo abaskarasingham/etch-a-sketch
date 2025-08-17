73193f9b4b5d95f1acd21f1504112f9a09bc34bb
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2025-08-17 19:06:17 -0400

    Add CSS styling and 1 row to etch-a-sketch
    The CSS styling will now make it look more like an
    etch-a-sketch. I have the border of the
    etch-a-sketch created, and have also made 1 row.
    
    Now, I have to fill in the remaining rows to make
    it look like a finished etch-a-sketch.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const container = document.querySelector(".container");
+const MAX_WIDTH = 960;
+let squares = 16;
 
-for (let i = 0; i < 256; ++i) {
+for (let i = 0; i < squares; ++i) {
     const div = document.createElement("div");
     container.appendChild(div);
 }
 
 const allDivs = Array.from(document.querySelectorAll(".container > div"));
+
 allDivs.forEach(div => {
+    div.setAttribute("style", `width: ${MAX_WIDTH / squares}px; height: ${MAX_WIDTH / squares}px;`);
+
     div.addEventListener("mouseenter", function(e) {
-        div.setAttribute("style", "background-color: greenyellow;");
+        div.setAttribute("style", `background-color: black; width: ${MAX_WIDTH / squares}px; height: ${MAX_WIDTH / squares}px;`);
     });
 });
