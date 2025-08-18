a6d8060249e4a2bce93f0375f61952296c9e6565
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2025-08-18 19:50:49 -0400

    Add working etch-a-sketch
    The etch-a-sketch now functions as expected.
    The amount of squares can be modified in the code
    to adjust the drawing space without changing the
    size of the canvas.
    
    Now I must add a button that sends a prompt, to
    allow the user to change the amount of squares on
    the canvas, and subsquently add a feature for
    dynamic colors.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const container = document.querySelector(".container");
-const MAX_WIDTH = 960;
-let squares = 16;
+const MAX_WIDTH = 700;
+let squares = 100;
+const dimensions = (MAX_WIDTH / squares);
 
-for (let i = 0; i < squares; ++i) {
+for (let i = 0; i < squares**2; ++i) {
     const div = document.createElement("div");
+    div.classList.add("square");
+    div.setAttribute("style", `width: ${dimensions}px; height: ${dimensions}px;`);
     container.appendChild(div);
 }
 
-const allDivs = Array.from(document.querySelectorAll(".container > div"));
+const allDivs = Array.from(document.querySelectorAll(".square"));
 
 allDivs.forEach(div => {
-    div.setAttribute("style", `width: ${MAX_WIDTH / squares}px; height: ${MAX_WIDTH / squares}px;`);
-
     div.addEventListener("mouseenter", function(e) {
-        div.setAttribute("style", `background-color: black; width: ${MAX_WIDTH / squares}px; height: ${MAX_WIDTH / squares}px;`);
+        div.setAttribute("style", `background-color: black; width: ${dimensions}px; height: ${dimensions}px;`);
+        // div.setAttribute("style", "background-color: black;");
     });
 });
