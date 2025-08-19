e6f669a1916db7086e538419c9943597f7d38f65
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2025-08-19 17:48:27 -0400

    Add button to change sketch pad size
    This button will ask the user to input the number
    of squares per side for the new grid, allowing
    them to modify the size of their sketch pad.

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 const container = document.querySelector(".container");
 const MAX_WIDTH = 700;
 
-let squares = 100;
-const totalSquares = squares**2;
-const dimensions = (MAX_WIDTH / squares);
+function generateGrid(input) {
+    let squares = input;
+    const totalSquares = squares ** 2;
+    const dimensions = (MAX_WIDTH / squares);
 
-for (let i = 0; i < totalSquares; ++i) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    div.style.width = `${dimensions}px`;
-    div.style.height = `${dimensions}px`;
-    
-    container.appendChild(div);
-}
+    for (let i = 0; i < totalSquares; ++i) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        div.style.width = `${dimensions}px`;
+        div.style.height = `${dimensions}px`;
 
-const allDivs = Array.from(document.querySelectorAll(".square"));
+        container.appendChild(div);
+    }
 
-allDivs.forEach(div => {
-    div.addEventListener("mouseenter", function(e) {
-        div.style.backgroundColor = "black";
+    const allDivs = Array.from(document.querySelectorAll(".square"));
+
+    allDivs.forEach(div => {
+        div.addEventListener("mouseenter", function (e) {
+            div.style.backgroundColor = "black";
+        });
     });
+}
+generateGrid(50);
+
+const button = document.querySelector("button");
+button.addEventListener("click", function(e) {
+    const input = prompt("Please enter the number of squares per side for the new grid.\n(1-100, inclusive)", container.childNodes.length**0.5);
+
+    if (input !== null && input !== "") {
+        if (isNaN(input)) {
+            alert("Please enter a valid number!");
+        } else {
+            if (input > 100) {
+                alert("Maximum number of squares is 100!");
+            } else {
+                container.replaceChildren();
+                (input > 0) ? generateGrid(Math.trunc(input)) : generateGrid(0);
+            }
+        }
+    }
 });
